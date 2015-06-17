f8c6588f15e59f91350d1287358070bbe3a34939
Jared Henderson <jared@netrivet.com>
2015-06-16 20:05:12 -0400

    add response class

--- src/Response.php
+++ src/Response.php
@@ -2,11 +2,82 @@
 
 namespace NetRivet\WordPress\Http;
 
+
 class Response
 {
 
+    /**
+     * @var int
+     */
+    protected $statusCode;
+
+    /**
+     * @param string  $body
+     * @param integer $statusCode
+     * @param array   $headers
+     * @throws \InvalidArgumentException
+     */
+    public function __construct($body = '', $statusCode = 200, array $headers = [])
+    {
+        if (! $this->statusCodeValid($statusCode)) {
+            throw new \InvalidArgumentException('Invalid status code');
+        }
+
+        $this->body = $body;
+        $this->statusCode = $statusCode;
+    }
+
+    /**
+     * Gets the response status code.
+     *
+     * The status code is a 3-digit integer result code of the server's attempt
+     * to understand and satisfy the request.
+     *
+     * @return int
+     */
     public function getStatusCode()
     {
-        
+        return $this->statusCode;
+    }
+
+    /**
+     * Gets the body of the response
+     *
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Get json-decoded body
+     *
+     * @return array
+     */
+    public function json()
+    {
+        $json = json_decode(strval($this->getBody()), true);
+
+        if (!is_array($json)) {
+            throw new \UnexpectedValueException('Body not json-encoded');
+        }
+
+        return $json;
+    }
+
+    /**
+     * Determine validity of input status code
+     *
+     * @param  mixed $statusCode
+     * @return bool
+     */
+    protected function statusCodeValid($statusCode)
+    {
+        if (!is_int($statusCode)) {
+            return false;
+        }
+
+        return $statusCode >= 100 && $statusCode < 600;
     }
 }
---
+++ tests/ResponseTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace NetRivet\WordPress\Http;
+
+
+class ResponseTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testThrowsExceptionWhenConstructedWithNonIntegerStatusCode()
+    {
+        new Response('', 'foobar');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testThrowsExceptionWhenConstructedWithInvalidStatusCode()
+    {
+        new Response('', 999);
+    }
+
+
+    public function testReturnsStatusCode()
+    {
+        $response = new Response('', 201);
+
+        $this->assertSame(201, $response->getStatusCode());
+    }
+
+
+    public function testGetBodyReturnsBody()
+    {
+        $response = new Response('foo');
+
+        $this->assertSame('foo', $response->getBody());
+    }
+
+    /**
+     * @expectedException \UnexpectedValueException
+     */
+    public function testJsonThrowsIfBodyNotJsonDecodable()
+    {
+        $response = new Response('foo');
+        $response->json();
+    }
+
+
+    public function testJsonReturnsJsonDecodedBody()
+    {
+        $response = new Response(json_encode(['foo' => 'bar']));
+        $json = $response->json();
+
+        $this->assertSame(['foo' => 'bar'], $json);
+    }
+}
