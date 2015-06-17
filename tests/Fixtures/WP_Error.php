9371e81307b5cf8a1d13e77190a9da12394fb84e
Jared Henderson <jared@netrivet.com>
2015-06-16 21:47:03 -0400

    remove autoloading of test fixtures

--- tests/Fixtures/WP_Error.php
+++ tests/Fixtures/WP_Error.php
@@ -4,11 +4,7 @@
  * This is just a test-fixture stub of the real WP_Error
  * so we can mock it for unit testing purposes. #YOLO
  */
-if (!class_exists('WP_Error')) {
-
 class WP_Error
 {
     public function get_error_messages(){}
-}
-
 }
--- tests/Fixtures/WP_Http.php
+++ tests/Fixtures/WP_Http.php
@@ -4,12 +4,8 @@
  * This is just a test-fixture stub of the real WP_Http
  * so we can mock it for unit testing purposes. #YOLO
  */
-if (!class_exists('WP_Http')) {
-
 class WP_Http
 {
     public function get(){}
     public function post(){}
-}
-
 }
