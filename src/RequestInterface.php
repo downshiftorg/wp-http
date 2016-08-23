ec1a41c6a2a0397d7a83cb2388db93017c4b12aa
Jared Henderson <jared@netrivet.com>
2016-08-23 16:34:20 -0400

    allow passing extra params like timeout to Request::postJson()

--- src/RequestInterface.php
+++ src/RequestInterface.php
@@ -9,37 +9,38 @@ interface RequestInterface
     /**
      * Send an http GET request
      *
-     * @param  string $uri
-     * @param  array  $params
-     * @return NetRivet\WordPress\Http\ResponseInterface
+     * @param string $uri
+     * @param array $params
+     * @return ResponseInterface
      */
     public function get($uri, array $params = []);
 
     /**
      * Send an http POST request
      *
-     * @param  string $uri
-     * @param  array  $params
-     * @return NetRivet\WordPress\Http\ResponseInterface
+     * @param string $uri
+     * @param array $params
+     * @return ResponseInterface
      */
     public function post($uri, array $params = []);
 
     /**
      * Send a json-encoded http POST request
      *
-     * @param  string $uri
-     * @param  array  $data
-     * @return NetRivet\WordPress\Http\ResponseInterface
+     * @param string $uri
+     * @param array $body
+     * @param array $params
+     * @return ResponseInterface
      */
-    public function postJson($uri, array $data);
+    public function postJson($uri, array $body, array $params = []);
 
     /**
      * Send an http request
      *
-     * @param  string $method
-     * @param  string $uri
-     * @param  array  $params
-     * @return NetRivet\WordPress\Http\ResponseInterface
+     * @param string $method
+     * @param string $uri
+     * @param array $params
+     * @return ResponseInterface
      */
     public function request($method, $uri, array $params = []);
 }
